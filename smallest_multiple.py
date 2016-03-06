89901a02b1f8bb4aaf6b229d44b1a1d1a467dd9f
carthick <challakarthik@gmail.com>
2016-03-06 17:51:21 +0530

    completed sum_square_difference

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -8,7 +8,7 @@ rangemax = 20
 check_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
      
 def find_solution(step):
-    for num in xrange(step, 999999999, 20):
+    for num in xrange(step, 999999999, step):
         if all(num % n == 0 for n in check_list):
             return num
     return None
--- sum_square_difference.py
+++ sum_square_difference.py
@@ -1,12 +1,31 @@
-'''The sum of the squares of the first ten natural numbers is,
+"""The sum of the squares of the first ten natural numbers is,
 
-12 + 22 + ... + 102 = 385
+1**2 + 2**2 + ... + 10**2 = 385
 The square of the sum of the first ten natural numbers is,
 
 (1 + 2 + ... + 10)2 = 552 = 3025
 Hence the difference between the sum of the squares of the first 
-ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ten natural numbers and the square of the sum is 
 
 Find the difference between the sum of the squares of the 
-first one hundred natural numbers and the square of the sum.'''
+first one hundred natural numbers and the square of the sum."""
 
+class diff():
+    def sum_of_square_of_first_hundred_natural_numbers(self):
+        count  =0 
+        for i in range(1,101):
+            count = count + i**2
+            
+        return  count
+    def square_of_sum_of_first_hundred_natural_numbers(self):
+        count  =0 
+        for i in range(1,101):
+            count = count + i
+        return count**2    
+
+dif = diff()
+sum_of_square = dif.sum_of_square_of_first_hundred_natural_numbers()
+square_of_sum = dif.square_of_sum_of_first_hundred_natural_numbers()
+            
+print square_of_sum-sum_of_square
+            
